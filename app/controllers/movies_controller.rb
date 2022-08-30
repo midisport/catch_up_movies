85a0e97c4456ea540de4d9f37a2dc01c4689cbb8
midisport <103263066+midisport@users.noreply.github.com>
2022-08-30 18:15:56 +0200

    home-first-batch

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,9 @@
 class MoviesController < ApplicationController
+  def index
+    if params[:query].present?
+      @movies = Movie.where(title: params[:query])
+    else
+      @movies = Movie.all
+    end
+  end
 end
