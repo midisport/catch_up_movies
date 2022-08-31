c34bd46e71ec30ca8a754ed8cd5570c94e45e9bc
Samuel Paget <sjapaget@gmail.com>
2022-08-31 17:04:50 +0200

    Add follows#index with basic view

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,2 +1,6 @@
 class FollowsController < ApplicationController
+  def index
+    @followers = Follow.where(followed: current_user) # follower_id = current_user
+    @followeds = Follow.where(follower: current_user) # follower_id = current_user
+  end
 end
