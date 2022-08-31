1ef699cfafaf9d43d16836e4575773c411fa6d2c
Samuel Paget <sjapaget@gmail.com>
2022-08-31 17:15:51 +0200

    Add skeleton for follows#create

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -3,4 +3,8 @@ class FollowsController < ApplicationController
     @followers = Follow.where(followed: current_user) # follower_id = current_user
     @followeds = Follow.where(follower: current_user) # follower_id = current_user
   end
+
+  def create
+    
+  end
 end
