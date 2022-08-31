b3200c23f24715577863fe771fecccb4d709f5a1
Samuel Paget <sjapaget@gmail.com>
2022-08-31 16:46:36 +0200

    Add uniqueness validation to follows to prevent following same user twice

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,6 @@
 class Follow < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
+
+  validates :follower, uniqueness: { scope: :followed, message: "Vous suivez déja cette personne" }
 end
