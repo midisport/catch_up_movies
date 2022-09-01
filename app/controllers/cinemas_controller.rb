3a08e786ab345b4e90c0944206341f735488a78d
midisport <103263066+midisport@users.noreply.github.com>
2022-09-01 15:06:57 +0200

    geo

--- app/controllers/cinemas_controller.rb
+++ app/controllers/cinemas_controller.rb
@@ -1,3 +1,14 @@
 class CinemasController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
 end
+
+def index
+  @cinemas = Cinema.all
+  # The `geocoded` scope filters only flats with coordinates
+  @markers = @cinemas.geocoded.map do |flat|
+    {
+      lat: flat.latitude,
+      lng: flat.longitude
+    }
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_01_101235) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_01_114645) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_101235) do
     t.datetime "updated_at", null: false
     t.float "lat"
     t.float "lng"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "comments", force: :cascade do |t|
