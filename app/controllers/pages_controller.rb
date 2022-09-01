a376163b3ca4d78f3b1b2cdf8deb0a7c49b79125
Samuel Paget <sjapaget@gmail.com>
2022-09-01 17:55:44 +0200

    Add render of follows and interests to dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,5 +8,11 @@ class PagesController < ApplicationController
     @follows = policy_scope(Follow)
     @followers = Follow.where(followed: current_user)
     @followeds = Follow.where(follower: current_user)
+
+    @interests = policy_scope(Interest)
+
+    @user = User.find(params[:id])
+    @interests = Interest.where(user: @user)
+    @follow = Follow.new
   end
 end
