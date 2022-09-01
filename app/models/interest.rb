53d5da7d46a1dd89250e136e3357d0e261bac799
Samuel Paget <sjapaget@gmail.com>
2022-09-01 14:51:33 +0200

    Add dependent destroy for interests

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,6 +1,6 @@
 class Interest < ApplicationRecord
-  belongs_to :user
-  belongs_to :movie
+  belongs_to :user, dependent: :destroy
+  belongs_to :movie, dependent: :destroy
 
   validates :user, :movie, presence: true
   validates :movie, uniqueness: { scope: :user, message: "Ce film est déja dans votre Watchlist !" }
