b3189491e337ebaab374275375a6383a4df2f08e
Samuel Paget <sjapaget@gmail.com>
2022-09-01 16:08:55 +0200

    Add empty booking to cinemas#show for bookings#create

--- app/controllers/cinemas_controller.rb
+++ app/controllers/cinemas_controller.rb
@@ -3,6 +3,8 @@ class CinemasController < ApplicationController
 
   def show
     @cinema = Cinema.find(params[:id])
+    @movie_shows = MovieShow.where(cinema_id: @cinema.id)
+    @booking = Booking.new
     authorize @cinema
   end
 end
