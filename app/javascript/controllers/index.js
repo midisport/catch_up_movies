0ea99cab2e491e6df21a67f9e34b1ff5144d2069
Youssef Aissaoui <aissa.youssef45@gmail.com>
2022-09-05 14:40:46 +0200

    add overflow&ajax

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,7 +7,11 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-import TabsController from "./tabs_controller"
-application.register("tabs", TabsController)
+import InsertInListController from "./insert_in_list_controller"
+application.register("insert-in-list", InsertInListController)
+
 import MapController from "./map_controller"
 application.register("map", MapController)
+
+import TabsController from "./tabs_controller"
+application.register("tabs", TabsController)
