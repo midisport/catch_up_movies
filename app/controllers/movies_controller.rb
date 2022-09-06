0425ca93244a0f0b9d768199bf3bbd7e5ef2dce6
midisport <103263066+midisport@users.noreply.github.com>
2022-09-06 11:05:02 +0200

    fixedmapposition

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -28,7 +28,7 @@ class MoviesController < ApplicationController
         lat: cinema.lat,
         lng: cinema.lng,
         info_window: render_to_string(partial: "info_window", locals: { cinema: cinema },),
-        image_url: helpers.asset_url("ecran-de-cinema.png")
+        image_url: helpers.asset_url("tickets-de-films.png")
       }
     end
   end
