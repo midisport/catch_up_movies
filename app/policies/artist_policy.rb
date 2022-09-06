8169f1c24ed66d831801dcfcf72d1c6a4f3d9547
Youssef Aissaoui <aissa.youssef45@gmail.com>
2022-09-06 13:11:28 +0200

    director show

--- app/policies/artist_policy.rb
+++ app/policies/artist_policy.rb
@@ -1,12 +1,12 @@
 class ArtistPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+     def resolve
+       scope.all
+     end
   end
 
-  def show?
+  def index?
     true
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -23,21 +23,20 @@
 # puts "destroying castings"
 # Casting.destroy_all
 #
-# # Users
-# puts "creating users"
-# user1 = User.create!(username: "Nabil", email: "test@test.fr", password: '123456')
-# user2 = User.create!(username: "Youssef", email: "test1@test.fr", password: '123456')
-# user3 = User.create!(username: "Samuel", email: "test2@test.fr", password: '123456')
-# user4 = User.create!(username: "Lazare", email: "test3@test.fr", password: '123456')
-#
-# # Follows
-# puts "creating follows"
-# Follow.create!(follower: user1, followed: user2)
-# Follow.create!(follower: user2, followed: user3)
-# Follow.create!(follower: user3, followed: user4)
-# Follow.create!(follower: user3, followed: user2)
-# Follow.create!(follower: user4, followed: user1)
-# puts "follows created"
+# Users
+puts "creating users"
+user1 = User.create!(username: "Nabil", email: "test@test.fr", password: '123456')
+user2 = User.create!(username: "Youssef", email: "test1@test.fr", password: '123456')
+user3 = User.create!(username: "Samuel", email: "test2@test.fr", password: '123456')
+user4 = User.create!(username: "Lazare", email: "test3@test.fr", password: '123456')
+# Follows
+puts "creating follows"
+Follow.create!(follower: user1, followed: user2)
+Follow.create!(follower: user2, followed: user3)
+Follow.create!(follower: user3, followed: user4)
+Follow.create!(follower: user3, followed: user2)
+Follow.create!(follower: user4, followed: user1)
+puts "follows created"
 #
 # # Movies
 # puts "creating movies"
